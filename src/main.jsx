ac67e40168c90a2eb1aaa5cc13be26fd0abeaf5e
Shakib <hossenmdshakib47@gmail.com>
2025-12-03 18:10:36 +0600

    add favicon and change menue link in navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import router from "./router/router.jsx";
 import AuthProvider from "./context/AuthContext/AuthProvider.jsx";
+import { RouterProvider } from "react-router";
+import "./App.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
